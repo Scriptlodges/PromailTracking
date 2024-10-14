f17907cd88d4abaf6844dd826d7a3f698f4cacc0
root <khasru.miah@selise.ch>
2024-10-14 16:04:44 +0600

    Initional commit

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Scriptlodge. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Scriptlodges_PromailTracking', __DIR__);
